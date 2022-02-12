713d5ba1962e81dd7e561eb23ba2ce58582c911b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-02-13 03:31:34 +0530

    Update ssh_connect.py

--- ssh_connect.py
+++ ssh_connect.py
@@ -1,40 +1,41 @@
-# import os
-# import paramiko
-# host = os.environ['TALOS_IP_ADDRESS']
-# port = os.environ['TALOS_PORT']
-# username = os.environ['TALOS_USER']
-# password = os.environ['TALOS_PASSWORD']
+
+import paramiko
+config_path="config.json"
+
+import json
+
+with open(config_path, 'r') as f:
+  data = json.load(f)
+  
+host = data['TALOS_IP_ADDRESS']
+port = data['TALOS_PORT']
+username = data['TALOS_USER']
+password = data['TALOS_PASSWORD']
+
+command = "ls"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+stdin, stdout, stderr = ssh.exec_command("python3 -V")
+lines = stdout.readlines()
+print(lines)
 
 
-# command = "ls"
+# def splitDictByVals(d,n_splits=2):
+#     dicts=[{} for i in range(n_splits)]
+#     def _chunkify(lst,n):
+#      return [lst[i::n] for i in range(n)]
+#     for k,v in d.items():
+#         for i in range(n_splits):
+#             dicts[i][k]=_chunkify(v, n_splits)[i]
+#     return dicts
 
-# ssh = paramiko.SSHClient()
-# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# ssh.connect(host, port, username, password)
+# print(splitDictByVals( {
+#     'first_neuron': [12, 24],
+#     'activation': ['relu', 'elu'],
+#     'batch_size': [10, 20]
+# }
 
-# stdin, stdout, stderr = ssh.exec_command(command)
-# lines = stdout.readlines()
-# print(lines)
-
-
-def splitDictByVals(d,n_splits=2):
-    dicts=[{} for i in range(n_splits)]
-    def _chunkify(lst,n):
-     return [lst[i::n] for i in range(n)]
-    for k,v in d.items():
-        for i in range(n_splits):
-            dicts[i][k]=_chunkify(v, n_splits)[i]
-    return dicts
-        
-   
-        
-        
-        
-
-
-print(splitDictByVals({
-    'first_neuron': [12, 24],
-    'activation': ['relu', 'elu','selu'],
-    'batch_size': [10, 20,12]
-}
-))
+# ))
