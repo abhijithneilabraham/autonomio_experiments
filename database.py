e68ec69cb76807bc9dd20033980fcb03d3531cbd
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-02-20 03:10:01 +0530

    update db experiments

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, types
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from sqlalchemy.exc import DatabaseError
 from sqlalchemy.schema import DropTable
-import awswrangler as wr
 import ast
 import pandas as pd
 
@@ -27,7 +26,7 @@ class Database:
     fetch_data_aws(question, query, db_type, username='', password='', database='', host='', port='', drop_db=True)
         Function that fetches the data for the query from the database on AWS.
     """
-    def __init__(self, data_dir, schema_dir, aws_s3, access_key_id, secret_access_key):
+    def __init__(self,username,password,host,port=None,db_type="postgres",database_name="experiment_log"):
         """
         Constructs all the necessary attributes for the database object.
 
@@ -41,10 +40,20 @@ class Database:
         data_process: `data_utils` class object
         nlp: `NLP` class object
         """
-        self.data_dir = data_dir
-        self.schema_dir = schema_dir
-   
-    def __create_db(self, db_url):
+
+        self.db_type=db_type
+        DB_URL=""
+        if db_type == 'sqlite':
+            DB_URL = 'sqlite://'
+        elif db_type == 'mysql':
+            if port is None:    port = 3306
+            DB_URL = 'mysql+pymysql://' + username + ':' + password + '@' + host + ':' + str(port)+'/'+database_name
+        elif db_type == 'postgres':
+            if port is None:    port = 5432
+            DB_URL = 'postgres://' + username + ':' + password + '@' + host + ':' + str(port) + '/' + database_name
+
+        self.DB_URL=DB_URL
+    def create_db(self, db_url):
         """
         Create database if it doesn't exists.
         """
@@ -62,34 +71,18 @@ class Database:
             raise Exception("Some error occured while connecting to the database.")
         return engine
 
-    def __drop_db(self, db_url):
+    def drop_db(self, db_url):
         """
         Drop the database.
         """
         drop_database(db_url)
 
-    def __delete_table(self, table_name):
+    def delete_table(self, table_name):
         """
         Delete the table.
         """
         DropTable(table_name)
-
-    # Function to load and fetch data from AWS storage.
-    def write(self, data_frame,db_type, username='', password='', database='', host='', port='', drop_db=True):
-
-        # dialect+driver://username:password@host:port/database
-        if db_type == 'postgres':
-            if port is None:    port = 5432     # use default port for postgres if it is None.
-            engine = wr.db.get_engine(db_type="postgresql", host=host, port=port, database=database, user=username, password=password)
-            # engine =wr.catalog.get_engine("aws-postgres-sql", db_type="postgresql", host=host, port=port, database=database, user=username, password=password)
-        elif db_type == 'mysql':
-            if port is None:    port = 3306
-            engine = wr.db.get_engine(db_type="mysql", host=host, port=port, database=database, user=username, password=password)
-
-
-        wr.db.to_sql(data_frame, engine, name="experiment_log", if_exists="add", index=False) 
-
-        # answer = wr.db.read_sql_query(query, con=engine)
-        # answer = answer.values.tolist()
-
-        # return answer
+        
+    def write_to_db(self, data_frame,username,password,host,port=None,db_type="postgres",database_name="experiment_log"):
+        engine = self.__create_db(self.DB_URL)
+        data_frame.to_sql(database_name, con=engine, if_exists='append')
