f16acec82143761ae0c6f428b991ef01281ff01f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-02-22 08:19:03 +0530

    database methods

--- database.py
+++ database.py
@@ -26,7 +26,7 @@ class Database:
     fetch_data_aws(question, query, db_type, username='', password='', database='', host='', port='', drop_db=True)
         Function that fetches the data for the query from the database on AWS.
     """
-    def __init__(self,username,password,host,port=None,db_type="postgres",database_name="experiment_log"):
+    def __init__(self,username,password,host,port=None,db_type="postgres",database_name="EXPERIMENT_LOG",table_name="experiment_log"):
         """
         Constructs all the necessary attributes for the database object.
 
@@ -42,6 +42,8 @@ class Database:
         """
 
         self.db_type=db_type
+        self.database_name=database_name
+        self.table_name=table_name
         DB_URL=""
         if db_type == 'sqlite':
             DB_URL = 'sqlite://'
@@ -50,15 +52,14 @@ class Database:
             DB_URL = 'mysql+pymysql://' + username + ':' + password + '@' + host + ':' + str(port)+'/'+database_name
         elif db_type == 'postgres':
             if port is None:    port = 5432
-            DB_URL = 'postgres://' + username + ':' + password + '@' + host + ':' + str(port) + '/' + database_name
-
+            DB_URL = 'postgresql://' + username + ':' + password + '@' + host + ':' + str(port) + '/' + database_name
         self.DB_URL=DB_URL
-    def create_db(self, db_url):
+    def create_db(self):
         """
         Create database if it doesn't exists.
         """
         try:
-            engine = create_engine(db_url, echo=False)
+            engine = create_engine(self.DB_URL, echo=False)
             if not database_exists(engine.url):
                 create_database(engine.url)
         except DatabaseError as e:
@@ -71,11 +72,11 @@ class Database:
             raise Exception("Some error occured while connecting to the database.")
         return engine
 
-    def drop_db(self, db_url):
+    def drop_db(self):
         """
         Drop the database.
         """
-        drop_database(db_url)
+        drop_database(self.DB_URL)
 
     def delete_table(self, table_name):
         """
@@ -83,6 +84,15 @@ class Database:
         """
         DropTable(table_name)
         
-    def write_to_db(self, data_frame,username,password,host,port=None,db_type="postgres",database_name="experiment_log"):
-        engine = self.__create_db(self.DB_URL)
-        data_frame.to_sql(database_name, con=engine, if_exists='append')
+    def write_to_db(self, data_frame):
+        engine = self.create_db()
+        data_frame.to_sql(self.table_name, con=engine, if_exists='append')
+    def query_table(self,query):
+        engine = self.create_db()
+        res=engine.execute(query).fetchall()
+        return res
+    def show_table_content(self):
+        res=self.query_table("SELECT * FROM {}".format(self.table_name))
+        return res
+        
+        
