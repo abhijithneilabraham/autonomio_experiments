0690c050ba4f6d8fda7bf645621a0d372934d675
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-03-11 16:32:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,73 +1,103 @@
-from sqlalchemy import create_engine, types
+from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from sqlalchemy.exc import DatabaseError
 from sqlalchemy.schema import DropTable
-import ast
-import pandas as pd
-
 
 
 class Database:
-    """
-    Fetches the query results from database.
-
-    Attributes
-    ----------
-    data_dir: `str` or `pathlib.Path` object
-            absolute path to folder containing all input files.
-    schema_dir: `str` or `pathlib.Path` object
-            path to folder containing `json` schemas of input files.
-            If not specified, auto-generated schema will be used.
-
-    Methods
-    -------
-    fetch_data(question, query, db_type, username='', password='', database='', host='', port='', drop_db=True)
-        Function that fetches the data for the query from the database on local system.
-    fetch_data_aws(question, query, db_type, username='', password='', database='', host='', port='', drop_db=True)
-        Function that fetches the data for the query from the database on AWS.
-    """
-    def __init__(self,username,password,host,port=None,db_type="postgres",database_name="EXPERIMENT_LOG",table_name="experiment_log"):
+    def __init__(
+        self,
+        username=None,
+        password=None,
+        host=None,
+        port=None,
+        db_type="sqlite",
+        database_name="EXPERIMENT_LOG",
+        table_name="experiment_log",
+        encoding="latin1"
+    ):
         """
-        Constructs all the necessary attributes for the database object.
 
-        Attributes
+        Parameters
         ----------
-        data_dir: `str` or `pathlib.Path` object
-                absolute path to folder containing all input files.
-        schema_dir: `str` or `pathlib.Path` object
-                path to folder containing `json` schemas of input files.
-                If not specified, auto-generated schema will be used.
-        data_process: `data_utils` class object
-        nlp: `NLP` class object
-        """
+        username : TYPE, optional
+            DESCRIPTION. The default is None.
+        password : TYPE, optional
+            DESCRIPTION. The default is None.
+        host : TYPE, optional
+            DESCRIPTION. The default is None.
+        port : TYPE, optional
+            DESCRIPTION. The default is None.
+        db_type : TYPE, optional
+            DESCRIPTION. The default is "sqlite".
+        database_name : TYPE, optional
+            DESCRIPTION. The default is "EXPERIMENT_LOG".
+        table_name : TYPE, optional
+            DESCRIPTION. The default is "experiment_log".
 
-        self.db_type=db_type
-        self.database_name=database_name
-        self.table_name=table_name
-        DB_URL=""
-        if db_type == 'sqlite':
-            DB_URL = 'sqlite://'
-        elif db_type == 'mysql':
-            if port is None:    port = 3306
-            DB_URL = 'mysql+pymysql://' + username + ':' + password + '@' + host + ':' + str(port)+'/'+database_name
-        elif db_type == 'postgres':
-            if port is None:    port = 5432
-            DB_URL = 'postgresql://' + username + ':' + password + '@' + host + ':' + str(port) + '/' + database_name
-        self.DB_URL=DB_URL
+        Returns
+        -------
+        None.
+
+        """
+        self.db_type = db_type
+        self.database_name = database_name
+        self.table_name = table_name
+        self.encoding=encoding
+        DB_URL = ""
+        if db_type == "sqlite":
+            DB_URL = "sqlite:///" + database_name + ".db"
+        elif db_type == "mysql":
+            if port is None:
+                port = 3306
+            DB_URL = (
+                "mysql+pymysql://"
+                + username
+                + ":"
+                + password
+                + "@"
+                + host
+                + ":"
+                + str(port)
+                + "/"
+                + database_name
+            )
+        elif db_type == "postgres":
+            if port is None:
+                port = 5432
+            DB_URL = (
+                "postgresql://"
+                + username
+                + ":"
+                + password
+                + "@"
+                + host
+                + ":"
+                + str(port)
+                + "/"
+                + database_name
+            )
+        self.DB_URL = DB_URL
+
     def create_db(self):
         """
         Create database if it doesn't exists.
         """
         try:
-            engine = create_engine(self.DB_URL, echo=False)
+            engine = create_engine(self.DB_URL, echo=False,encoding=self.encoding)
             if not database_exists(engine.url):
                 create_database(engine.url)
+                
         except DatabaseError as e:
             import traceback
+
             traceback.print_exc()
-            raise Exception("Check whether the server is running and the connection parameters are correct.")        
+            raise Exception(
+                "Check whether the server is running and the connection parameters are correct."
+            )
         except Exception as e:
             import traceback
+
             traceback.print_exc()
             raise Exception("Some error occured while connecting to the database.")
         return engine
@@ -83,16 +113,52 @@ class Database:
         Delete the table.
         """
         DropTable(table_name)
-        
+
     def write_to_db(self, data_frame):
+        """
+
+
+        Parameters
+        ----------
+        data_frame : TYPE: `DataFrame`
+            DESCRIPTION. DataFrame object consisting of tabular data.
+
+        Returns
+        -------
+        None.
+
+        """
         engine = self.create_db()
-        data_frame.to_sql(self.table_name, con=engine, if_exists='append')
-    def query_table(self,query):
+        data_frame.to_sql(self.table_name, con=engine, if_exists="append")
+
+    def query_table(self, query):
+        """
+
+
+        Parameters
+        ----------
+        query : TYPE: `str`
+            DESCRIPTION. Database query for the respective sql engine
+
+        Returns
+        -------
+        res : TYPE: `list` of `tuples`
+            DESCRIPTION. Query output from the database
+
+        """
         engine = self.create_db()
-        res=engine.execute(query).fetchall()
+        res = engine.execute(query).fetchall()
         return res
+
     def show_table_content(self):
-        res=self.query_table("SELECT * FROM {}".format(self.table_name))
+        """
+
+
+            Returns
+            -------
+        res : TYPE: `list` of `tuples`
+            DESCRIPTION. Query output from the database
+
+        """
+        res = self.query_table("SELECT * FROM {}".format(self.table_name))
         return res
-        
-        
