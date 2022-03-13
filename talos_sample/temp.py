4fa1f4b4bb23c5f7d55479926887f5d57a7903b5
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-03-13 09:58:08 +0530

    Update temp.py

--- talos_sample/temp.py
+++ talos_sample/temp.py
@@ -1,12 +1,15 @@
-
 from talos import DistributeScan
 
-p = {
-    'first_neuron': [12, 24],
-    'activation': ['relu', 'elu'],
-    'batch_size': [10, 20]
-}
-d=DistributeScan(params=p,config='config.json',file_path="talos_test.py",experiment_name="diabetes_exp")
-print(d.distributed_run(run_local=False,show_results=True))
+p = {'first_neuron': [12, 24],
+     'activation': ['relu', 'elu'], 
+     'batch_size': [10, 20]}
 
+d = DistributeScan(
+    params=p,
+    config='config.json',
+    file_path='talos_test.py',
+    experiment_name='diabetes_exp',
+)
 
+print(d.distributed_run(run_central_node=False,
+                        show_results=True))
